ebc8f1a99cac56d7a1b2b673ab485fcd1f758745
SergiBadia <Badrog90@gmail.com>
2016-11-20 16:07:36 +0100

    Añadido Practicas

---
+++ Practica_2(Isabel Lieto)/Calculadora/suma.js
@@ -0,0 +1,12 @@
+function suma(operacion){
+
+switch(operacion){
+	
+	case 1:document.getElementbyId("num3").value = parseint(document.getElementbyId("num1").value) + parseint(document.getElementbyId("num2").value);break;
+	case 2:document.getElementbyId("num3").value = parseint(document.getElementbyId("num1").value) + parseint(document.getElementbyId("num2").value);break;
+	case 3:document.getElementbyId("num3").value = parseint(document.getElementbyId("num1").value) + parseint(document.getElementbyId("num2").value);break;
+	case 4:document.getElementbyId("num3").value = parseint(document.getElementbyId("num1").value) + parseint(document.getElementbyId("num2").value);break;
+}
+
+
+}
